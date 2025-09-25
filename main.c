487eb9679b3c4a39d75b5c0edd87a8dd7fa94d4d
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2025-09-25 11:45:55 +0200

    feat: add support for number item array

--- main.c
+++ main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
+#define ARRAY_CAPACITY 10
+
+void print_contents(int items[ARRAY_CAPACITY], int count) {
+    if (count == 0) {
+        printf("Items are empty.\n");
+    }
+    for (int index = 0; index < count; index++) {
+        printf("item[%d] = %d\n", index, items[index]);
+    }
+}
+
 int main(void) {
-    printf("Hello World\n");
+    int numbers[ARRAY_CAPACITY] = {0};
+    int numbersCount = 0;
+
+    print_contents(numbers, numbersCount);
+
+    numbers[numbersCount++] = 10;
+    numbers[numbersCount++] = 30;
+    numbers[numbersCount++] = 50;
+
+    print_contents(numbers, numbersCount);
+
+    numbersCount--;
+    print_contents(numbers, numbersCount);
+
     return 0;
 }
