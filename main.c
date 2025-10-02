ab6bffe9f81712f88d4fc981712fba7fa5a939db
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2025-10-02 11:49:32 +0200

    feat: add list persistence

--- main.c
+++ main.c
@@ -1,30 +1,45 @@
 #include <stdio.h>
 
 #define ARRAY_CAPACITY 10
+#define FILE_NAME "list.txt"
+#define FILE_ARRAY_COUNT_TEMPLATE "There are %d items.\n"
 
-void print_contents(int items[ARRAY_CAPACITY], int count) {
-    if (count == 0) {
-        printf("Items are empty.\n");
-    }
+void print_contents(FILE *file, int items[ARRAY_CAPACITY], int count) {
+    fprintf(file, FILE_ARRAY_COUNT_TEMPLATE, count);
     for (int index = 0; index < count; index++) {
-        printf("item[%d] = %d\n", index, items[index]);
+        fprintf(file, "item[%d] = %d\n", index, items[index]);
     }
 }
 
+int load_contents(FILE *file, int items[ARRAY_CAPACITY]) {
+    int loadedCount;
+    fscanf(file, FILE_ARRAY_COUNT_TEMPLATE, &loadedCount);
+    for (int line = 0; line < loadedCount; line++) {
+        int scanned = fscanf(file, "item[%*d] = %d\n", &items[line]);
+        printf("scanf() == %d\n", scanned);
+    }
+
+    return loadedCount;
+}
+
+
 int main(void) {
+    FILE *file;
     int numbers[ARRAY_CAPACITY] = {0};
     int numbersCount = 0;
 
-    print_contents(numbers, numbersCount);
+    file = fopen(FILE_NAME, "r");
+    if (file != NULL) {
+        numbersCount = load_contents(file, numbers);
+        fclose(file);
+    }
 
     numbers[numbersCount++] = 10;
-    numbers[numbersCount++] = 30;
-    numbers[numbersCount++] = 50;
+    print_contents(stdout, numbers, numbersCount);
 
-    print_contents(numbers, numbersCount);
-
-    numbersCount--;
-    print_contents(numbers, numbersCount);
+    file = fopen(FILE_NAME, "w");
+    print_contents(file, numbers, numbersCount);
+    fclose(file);
 
     return 0;
 }
