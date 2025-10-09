63a3facb7df27862521cf9e95d23cb07d8b46cbc
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2025-10-09 11:51:59 +0200

    Sturcture magic + Dynamic allocations

--- main.c
+++ main.c
@@ -1,45 +1,69 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define ARRAY_CAPACITY 10
 #define FILE_NAME "list.txt"
-#define FILE_ARRAY_COUNT_TEMPLATE "There are %d items.\n"
+#define FILE_ARRAY_COUNT_TEMPLATE "There are %d/%d items.\n"
 
-void print_contents(FILE *file, int items[ARRAY_CAPACITY], int count) {
-    fprintf(file, FILE_ARRAY_COUNT_TEMPLATE, count);
-    for (int index = 0; index < count; index++) {
-        fprintf(file, "item[%d] = %d\n", index, items[index]);
+struct ItemArray {
+    int *items;
+    int capacity;
+    int currentCount;
+};
+
+void print_contents(FILE *file, struct ItemArray *array) {
+    fprintf(file, FILE_ARRAY_COUNT_TEMPLATE, array->currentCount, array->capacity);
+    for (int index = 0; index < array->currentCount; index++) {
+        fprintf(file, "item[%d] = %d\n", index, array->items[index]);
     }
 }
 
-int load_contents(FILE *file, int items[ARRAY_CAPACITY]) {
-    int loadedCount;
-    fscanf(file, FILE_ARRAY_COUNT_TEMPLATE, &loadedCount);
-    for (int line = 0; line < loadedCount; line++) {
-        int scanned = fscanf(file, "item[%*d] = %d\n", &items[line]);
+void load_contents(FILE *file, struct ItemArray *array) {
+    fscanf(file, FILE_ARRAY_COUNT_TEMPLATE, &(*array).currentCount, &array->capacity);
+    array->items = malloc(sizeof(*array->items) * array->capacity);
+    if (array->items == NULL) {
+        printf("ERR: Failed to allocate memory");
+        array->capacity = 0;
+        array->currentCount = 0;
+    }
+
+    for (int line = 0; line < array->currentCount; line++) {
+        int scanned = fscanf(file, "item[%*d] = %d\n", &array->items[line]);
         printf("scanf() == %d\n", scanned);
     }
-
-    return loadedCount;
 }
 
+void add_item(struct ItemArray *array, int value) {
+    if (array->capacity <= array->currentCount + 1) {
+        array->items = realloc(array->items, sizeof(*array->items) * array->capacity * 2);
+    }
+
+    array->items[array->currentCount++] = value;
+}
 
 int main(void) {
     FILE *file;
-    int numbers[ARRAY_CAPACITY] = {0};
-    int numbersCount = 0;
+    struct ItemArray numbers = {
+        .items = NULL,
+        .capacity = 0,
+        .currentCount = 0,
+    };
 
     file = fopen(FILE_NAME, "r");
     if (file != NULL) {
-        numbersCount = load_contents(file, numbers);
+        load_contents(file, &numbers);
         fclose(file);
+    } else {
+        return 1;
     }
 
-    numbers[numbersCount++] = 10;
-    print_contents(stdout, numbers, numbersCount);
+    add_item(&numbers, 10);
+    print_contents(stdout, &numbers);
 
     file = fopen(FILE_NAME, "w");
-    print_contents(file, numbers, numbersCount);
+    print_contents(file, &numbers);
     fclose(file);
 
+    free(numbers.items);
     return 0;
 }
